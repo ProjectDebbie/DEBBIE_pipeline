95a78d36db1163b9e3da223276f39148c8cae76c
jcorvi <javier.corvi@bsc.es>
2020-08-12 18:50:44 +0200

    remove old files

--- env_variable.sh
+++ env_variable.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-export MONGO_URL="mongodb://***REMOVED***:***REMOVED***@***REMOVED***:27017/?authSource=DEBBIE&authMechanism=SCRAM-SHA-1"
+export MONGO_URL="mongodb://OHakimi:deb2020..@mdb-login.bsc.es:27017/?authSource=DEBBIE&authMechanism=SCRAM-SHA-1"
