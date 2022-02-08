8928f7b9f2c37139d66ef4d318f019fdc1338832
javicorvi <javicorvi@gmail.com>
2022-02-08 13:50:11 +0100

    Update run_pubmed_with_timed_retrieval.sh

--- run_pubmed_with_timed_retrieval.sh
+++ run_pubmed_with_timed_retrieval.sh
@@ -2,30 +2,34 @@
 #set pipeline name
 pipeline_name=debbie_pipeline_`date '+%d-%m-%Y_%H_%M_%S'`
 #set the nextflow instalation
-nextflow_path="/home/jcorvi/nextflow_installation/nextflow"
+nextflow_path="/home/jcorvi/nextflow/nextflow"
 #set the pipeline file
-pipeline_path="/home/jcorvi/projects/debbie/DEBBIE_pipeline/pipeline_with_timed_retrieval.nf"
+pipeline_path="./pipeline_with_timed_retrieval.nf"
 #set the home/work dir of the pipeline
-base_dir="/home/jcorvi/DEBBIE_DATA/pipeline_complete/production_with_search/"`date '+%d-%m-%Y'`
+base_dir="/home/jcorvi/DEBBIE_DATA/pipeline_complete/production_with_mesh_search/"`date '+%d-%m-%Y'`
+#Registry file for processed abstracts. To avoid duplication of downloads during the update of the database.
+pubmedRegistryFile="/home/jcorvi/DEBBIE_DATA/pipeline_complete/production_with_mesh_search/debbie_standardization_list_files_processed.dat"
+pubmedRelDate=10
 #quert to search in pubmed
-searchQuery=silk
+searchQuery="((((((((Biomedical and dental materials[MeSH Terms]) OR (Prostheses and implants[MeSH Terms])) OR (Materials testing[MeSH Terms])) OR (Tissue engineering[MeSH Terms])) OR (Tissue scaffolds[MeSH Terms])) OR (Equipment safety[MeSH Terms])) OR (Medical device recalls[MeSH Terms])) OR (Biomaterials)) OR (Cell scaffolds)"
 
 
 
 #Database connection details
 #set server and port
-db_server=xxxx
-db_port=xxxx
+db_server=xxxxx
+db_port=xxxxx
 #set user and password
 db_user=xxxxx
-db_password=xxxxx
+db_password=xxxx
 #set the database name
 db_name=xxxxx
 #set the collection prefix
-db_collection_prefix=xxxx
+db_collection_prefix=xxxxxx
 
 #The complete mongo uri
 db_uri="mongodb://"$db_user":"$db_password"@"$db_server":"$db_port"/?authSource="$db_name"&authMechanism=SCRAM-SHA-1"
 
 #command
-$nextflow_path run $pipeline_path --baseDir $base_dir --db_uri $db_uri --db_name $db_name --db_collection_prefix $db_collection_prefix --searchQuery $searchQuery -with-report $base_dir/report.html -with-trace -with-timeline $base_dir/timeline.html -name $pipeline_name
+#remove searchQuery
+$nextflow_path run $pipeline_path --baseDir $base_dir --db_uri $db_uri --db_name $db_name --db_collection_prefix $db_collection_prefix --pubmedRelDate $pubmedRelDate --pubmedRegistryFile $pubmedRegistryFile -with-report $base_dir/report.html -with-trace -with-timeline $base_dir/timeline.html -name $pipeline_name
